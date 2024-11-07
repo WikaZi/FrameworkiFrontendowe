a1ab99a3f208eb65ab7e4226dbbd93fa19e4875f
WikaZi <142423027+WikaZi@users.noreply.github.com>
2024-11-07 17:46:13 +0100

    5.3 done

--- lab1/vite-project/src/components/TableHeader.jsx
+++ lab1/vite-project/src/components/TableHeader.jsx
@@ -1,12 +1,60 @@
+import { Dropdown } from 'react-bootstrap'; // Importujemy komponent Dropdown
 
-const TableHeader = () => {
+const TableHeader = ({ onSortUser, onSortTitle, onSortComments }) => {
     return (
         <thead>
+            
             <tr>
-                <th>User</th>
-                <th>Post Title</th>
-                <th>Comments</th>
+                <th>
+                    <tr>
+                        <td>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-user-sort">
+                            Sort By User Name
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => onSortUser('asc')}>Ascending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortUser('desc')}>Descending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortUser(null)}>Natural Order</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </td>
+                    </tr>
+                </th>
+                <th>
+                    <tr> 
+                        <td>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-title-sort">
+                            Sort By Title
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => onSortTitle('asc')}>Ascending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortTitle('desc')}>Descending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortTitle(null)}>Natural Order</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </td>
+                    </tr>
+                </th>
+                <th>
+                    <td>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-comments-sort">
+                            Sort By Comments
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => onSortComments('asc')}>Ascending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortComments('desc')}>Descending</Dropdown.Item>
+                            <Dropdown.Item onClick={() => onSortComments(null)}>Natural Order</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </td>
+                </th>
             </tr>
+
+            
+           
         </thead>
     );
 };
--- lab1/vite-project/src/pages/Lab5Page.jsx
+++ lab1/vite-project/src/pages/Lab5Page.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-import useFetch from '../data/useFetch';  
+import  { useState } from 'react';
+import useFetch from '../data/useFetch';  // Importujemy hook useFetch
 import { Link } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap'; 
-import TableHeader from '../components/TableHeader';  
+import TableHeader from '../components/TableHeader';  // Komponent TableHeader
 
 const Lab5Page = () => {
-    
+    // Pobieramy dane za pomocą hooka useFetch
     const [posts] = useFetch("https://jsonplaceholder.typicode.com/posts");
     const [users] = useFetch("https://jsonplaceholder.typicode.com/users");
     const [comments] = useFetch("https://jsonplaceholder.typicode.com/comments");
 
-    
+    // Przetwarzamy dane do jednej tabeli
     const tableData = posts.map((p) => {
         return {
             user: users.find((u) => u.id === p.userId),
@@ -19,42 +18,62 @@ const Lab5Page = () => {
         };
     });
 
-    
+    // Stan do przechowywania posortowanych danych
     const [sortedPosts, setSortedPosts] = useState(tableData);
 
-   
-    const sortPosts = (order) => {
+    // Funkcja sortująca dane po tytule posta
+    const sortPostsByTitle = (order) => {
         let sorted = [...tableData];
         if (order === 'asc') {
-            sorted.sort((a, b) => a.post.title.localeCompare(b.post.title)); 
+            sorted.sort((a, b) => a.post.title.localeCompare(b.post.title)); // Sortowanie rosnąco po tytule
         } else if (order === 'desc') {
-            sorted.sort((a, b) => b.post.title.localeCompare(a.post.title)); 
+            sorted.sort((a, b) => b.post.title.localeCompare(a.post.title)); // Sortowanie malejąco po tytule
         } else {
-            sorted = tableData; 
+            sorted = tableData; // Przywracamy naturalną kolejność
+        }
+        setSortedPosts(sorted);
+    };
+
+    // Funkcja sortująca dane po użytkowniku
+    const sortPostsByUser = (order) => {
+        let sorted = [...tableData];
+        if (order === 'asc') {
+            sorted.sort((a, b) => a.user.name.localeCompare(b.user.name)); // Sortowanie rosnąco po nazwie użytkownika
+        } else if (order === 'desc') {
+            sorted.sort((a, b) => b.user.name.localeCompare(a.user.name)); // Sortowanie malejąco po nazwie użytkownika
+        } else {
+            sorted = tableData; // Przywracamy naturalną kolejność
+        }
+        setSortedPosts(sorted);
+    };
+
+    // Funkcja sortująca dane po liczbie komentarzy
+    const sortPostsByComments = (order) => {
+        let sorted = [...tableData];
+        if (order === 'asc') {
+            sorted.sort((a, b) => a.comments.length - b.comments.length); // Sortowanie rosnąco po liczbie komentarzy
+        } else if (order === 'desc') {
+            sorted.sort((a, b) => b.comments.length - a.comments.length); // Sortowanie malejąco po liczbie komentarzy
+        } else {
+            sorted = tableData; // Przywracamy naturalną kolejność
         }
         setSortedPosts(sorted);
     };
 
     return (
-        <div>
+        <div className="container mt-4">
             <h1>Lab 5 - Data from JSONPlaceholder</h1>
-            <Link to="/lab4">Go Back to Previous Page</Link>
-
-            
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Sort By Title
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => sortPosts('asc')}>Ascending</Dropdown.Item>
-                    <Dropdown.Item onClick={() => sortPosts('desc')}>Descending</Dropdown.Item>
-                    <Dropdown.Item onClick={() => sortPosts(null)}>Natural Order</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+            <Link to="/lab4" className="btn btn-secondary mb-4">Go Back to Previous Page</Link>
 
            
-            <table className="table mt-4">
-                <TableHeader />  
+           
+            
+            <table className="table table-bordered">
+                <TableHeader 
+                onSortUser={sortPostsByUser} 
+                onSortTitle={sortPostsByTitle} 
+                onSortComments={sortPostsByComments} 
+                />
                 <tbody>
                     {sortedPosts.map((item, index) => (
                         <tr key={index}>
